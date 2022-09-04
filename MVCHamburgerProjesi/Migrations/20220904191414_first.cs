e82bd8c030d8d2c1b9441cddbe443898bb3dc54a
Kadir-Yildiz <105437364+Kadir-Yildiz@users.noreply.github.com>
2022-09-04 23:07:39 +0300

    MenüEkstra

---
+++ MVCHamburgerProjesi/Migrations/20220904191414_first.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MVCHamburgerProjesi.Migrations
+{
+    public partial class first : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Menuler",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MenuAd = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Fiyat = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Menuler", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Siparisler",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Boyut = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Adet = table.Column<int>(type: "int", nullable: false),
+                    ToplamTutar = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    SeciliMenuId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Siparisler", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Siparisler_Menuler_SeciliMenuId",
+                        column: x => x.SeciliMenuId,
+                        principalTable: "Menuler",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ekstralar",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EkstraAd = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Fiyat = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    SiparisId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ekstralar", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Ekstralar_Siparisler_SiparisId",
+                        column: x => x.SiparisId,
+                        principalTable: "Siparisler",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ekstralar_SiparisId",
+                table: "Ekstralar",
+                column: "SiparisId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Siparisler_SeciliMenuId",
+                table: "Siparisler",
+                column: "SeciliMenuId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ekstralar");
+
+            migrationBuilder.DropTable(
+                name: "Siparisler");
+
+            migrationBuilder.DropTable(
+                name: "Menuler");
+        }
+    }
+}
---
+++ MVCHamburgerProjesi/Models/MenuEkstraViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MVCHamburgerProjesi.Models
+{
+    public class MenuEkstraViewModel
+    {
+        public string EkstraAd { get; set; }
+        public decimal Fiyat { get; set; }
+        public string MenuAd { get; set; }
+    }
+}
