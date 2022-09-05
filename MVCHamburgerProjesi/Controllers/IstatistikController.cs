ef9a63118dcc343703a610f6e17d2facb8802362
Kadir-Yildiz <105437364+Kadir-Yildiz@users.noreply.github.com>
2022-09-05 22:23:34 +0300

    CRUD

---
+++ MVCHamburgerProjesi/Controllers/IstatistikController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCHamburgerProjesi.Controllers
+{
+    public class IstatistikController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- MVCHamburgerProjesi/Controllers/MenuEkstraController.cs
+++ MVCHamburgerProjesi/Controllers/MenuEkstraController.cs
@@ -1,12 +1,63 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVCHamburgerProjesi.Models;
 
 namespace MVCHamburgerProjesi.Controllers
 {
 	public class MenuEkstraController : Controller
 	{
+        private readonly ApplicationDbContext _db;
+
+        public MenuEkstraController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
 		public IActionResult Index()
 		{
+            ViewBag.Menuler = _db.Menuler.ToList();
+            ViewBag.Ekstralar = _db.Ekstralar.ToList();
 			return View();
 		}
-	}
+        
+
+        [HttpPost]
+        public IActionResult MenuEkle(MenuEkstraViewModel vm)
+        {
+            Menu menu = new Menu();
+            menu.MenuAd = vm.MenuAd;
+            menu.Fiyat = vm.Fiyat;
+            _db.Menuler.Add(menu);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult EkstraEkle(MenuEkstraViewModel vm)
+        {
+            Ekstra ekstra = new Ekstra();
+            ekstra.EkstraAd = vm.EkstraAd;
+            ekstra.Fiyat = vm.Fiyat;
+            _db.Ekstralar.Add(ekstra);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+        public IActionResult MenuSil(int id)
+        {
+            var menu = _db.Menuler.Where(x=>x.Id == id).FirstOrDefault();
+            _db.Menuler.Remove(menu);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+        public IActionResult EkstraSil(int id)
+        {
+            var ekstra = _db.Ekstralar.Where(x => x.Id == id).FirstOrDefault();
+            _db.Ekstralar.Remove(ekstra);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
 }
+
