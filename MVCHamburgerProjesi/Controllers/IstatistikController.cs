810503a90706f7d35309a7be15ce19306abe8224
Kadir-Yildiz <105437364+Kadir-Yildiz@users.noreply.github.com>
2022-09-10 00:56:55 +0300

    Istatistik

--- MVCHamburgerProjesi/Controllers/IstatistikController.cs
+++ MVCHamburgerProjesi/Controllers/IstatistikController.cs
@@ -4,9 +4,28 @@ namespace MVCHamburgerProjesi.Controllers
 {
     public class IstatistikController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public IstatistikController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            ViewBag.Ciro = _db.Siparisler.Sum(x=>x.ToplamTutar).ToString("C");
+            ViewBag.SiparişAdedi = _db.Siparisler.Count();
+            ViewBag.MenuAdedi = _db.Siparisler.Sum(x=>x.Adet);
+            var list = _db.Siparisler.ToList();
+            int toplam = 0;
+            foreach (var item in list)
+            {
+                if (item.Ekstralar!=null)
+                {
+                toplam += item.Ekstralar.Split(',').Count();
+                }
+            }
+            ViewBag.Ekstralar = toplam;
+            return View(_db.Siparisler.Include(x=>x.SeciliMenu).ToList());
         }
     }
 }
